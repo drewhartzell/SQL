c8b5ff835428dfb7293826296767779451f303bf
Drew Hartzell <151415194+drewhartzell@users.noreply.github.com>
2025-06-04 02:13:55 -0400

    Update Medicaid Query.sql

--- Snowflake Folder/Medicaid Folder/Medicaid Query.sql
+++ Snowflake Folder/Medicaid Folder/Medicaid Query.sql
@@ -1,21 +1,44 @@
-# Etsy Database #
+// Medicaid Database //
 
-SELECT
-    SHIP_STATE,
-    SUM(GROSS_AMOUNT),
-    SUM(NET_AMOUNT)
-FROM ITEMS
-JOIN SALES
-    ON ITEMS.ORDER_ID = SALES.ORDER_ID
-GROUP BY SHIP_STATE
-ORDER BY SUM(NET_AMOUNT) DESC
+SELECT 
+    PRODUCT_NAME,
+    COUNT(*)
+FROM DRUG_UTIL
+GROUP BY PRODUCT_NAME
+ORDER BY COUNT(*) DESC
 
-SELECT
-  YEAR(ORDER_DATE),
-  SUM(GROSS_AMOUNT),
-  SUM(FEES),
-  SUM(NET_AMOUNT)
-FROM
-  ETSY.PUBLIC.SALES
-GROUP BY YEAR(ORDER_DATE)
-ORDER BY YEAR(ORDER_DATE) DESC
+//
+//
+
+SELECT 
+    PRODUCT_NAME,
+    SUM(NUMBER_OF_PRESCRIPTIONS),
+    SUM(total_amount_reimbursed),
+    SUM(medicaid_amount_reimbursed)
+FROM DRUG_UTIL
+GROUP BY PRODUCT_NAME
+ORDER BY SUM(total_amount_reimbursed) DESC
+
+//
+//
+
+SELECT 
+    YEAR,
+    SUM(total_amount_reimbursed),
+    SUM(number_of_prescriptions)
+FROM DRUG_UTIL
+GROUP BY YEAR
+ORDER BY SUM(total_amount_reimbursed) DESC
+
+//
+//
+
+SELECT 
+    PRODUCT_NAME,
+    SUM(total_amount_reimbursed),
+    SUM(MEDICAID_AMOUNT_REIMBURSED),
+    SUM(number_of_prescriptions)
+FROM DRUG_UTIL
+GROUP BY PRODUCT_NAME
+ORDER BY SUM(total_amount_reimbursed) DESC
+LIMIT 10
