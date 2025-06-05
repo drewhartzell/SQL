ae607f8b09ebb5cfda63d00f212f93b3a63e1902
Drew Hartzell <151415194+drewhartzell@users.noreply.github.com>
2025-06-05 00:30:05 -0400

    Create Drop Database.sql

---
+++ Data Management Folder/Drop Database.sql
@@ -0,0 +1,27 @@
+# Etsy #
+
+# DROP Logic #
+
+DROP DATABASE IF EXISTS `sql_invoicing`;
+CREATE DATABASE `sql_invoicing`; 
+USE `sql_invoicing`;
+//
+//
+
+DROP DATABASE IF EXISTS `sql_store`;
+CREATE DATABASE `sql_store`;
+USE `sql_store`;
+
+//
+//
+
+DROP DATABASE IF EXISTS `sql_hr`;
+CREATE DATABASE `sql_hr`;
+USE `sql_hr`;
+
+//
+//
+
+DROP DATABASE IF EXISTS `sql_inventory`;
+CREATE DATABASE `sql_inventory`;
+USE `sql_inventory`;
